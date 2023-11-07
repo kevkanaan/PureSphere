0e21d984c4f71524217c57d26318745d05c4c3de
kevkanaan <64862904+kevkanaan@users.noreply.github.com>
2023-11-07 13:03:49 +0100

    water-api: update README and add function to get analysis by year (#8)

--- dags/water-api/water_quality.py
+++ dags/water-api/water_quality.py
@@ -1,4 +1,6 @@
+from calendar import monthrange
 import requests
+import pandas as pd
 
 def get_analysepc(params=None):
     """
@@ -11,15 +13,16 @@ def get_analysepc(params=None):
         Status code of the response.
     """
     endpoint="https://hubeau.eaufrance.fr/api/v2/qualite_rivieres/analyse_pc.csv"
-    response = requests.get(endpoint, params=params, timeout=10)
+    response = requests.get(endpoint, params=params)
     response.raise_for_status()
+    #print(response.headers)
 
     # Write the response content to a CSV file
     try:
-        with open('./data/water-quality/analysispc.csv', 'w', newline='', encoding='utf-8') as csvfile:
+        with open('./data/water-quality/analysispc.csv', 'wb', newline='', encoding='utf-8') as csvfile:
             csvfile.write(response.content.decode('utf-8'))
-    except FileNotFoundError:
-        print("Error while writing the file")
+    except FileNotFoundError as e:
+        print("Error while writing the file", str(e))
 
     return response.status_code
 
@@ -89,6 +92,54 @@ def get_analysepc_location(latitude, longitude, distance):
     }
     return get_analysepc(params)
 
+def get_analysepc_filtered_year(year, chemical_components):
+    """
+    Makes a GET request to the specified endpoint with optional query parameters for each month of the year and combines the results into a single CSV file.
+
+    Args:
+        year (int): Year of the measurement.
+        chemical_components (str): Chemical components of the measurement.
+
+    Returns:
+        Status code of the last response.
+    """
+    # Initialize an empty DataFrame to store the results
+    df = pd.DataFrame()
+
+    # Loop over each month of the year
+    for month in range(1, 13):
+        print(f"Processing month {month}...")
+        # Get the last day of the month
+        _, last_day = monthrange(year, month)
+
+        # Format the month and last_day to always be two digits
+        month = str(month).zfill(2)
+        last_day = str(last_day).zfill(2)
+
+        params = {
+            "date_debut_prelevement": f"{year}-{month}-01",
+            "date_fin_prelevement": f"{year}-{month}-{last_day}",
+            "code_parametre": chemical_components,
+            "sort": "desc",
+            "size": 20000
+        }
+
+        # Make the GET request
+        status_code = get_analysepc(params)
+        print(f"Response code is {status_code}")
+        # Read the results into a DataFrame
+        df_month = pd.read_csv('./data/water-quality/analysispc.csv', encoding='ISO-8859-1', sep=';')
+
+        # Append the results to the main DataFrame
+        df = pd.concat([df, df_month], ignore_index=True)
+
+        print(f"Finished processing month {month}")
+
+    # Write the main DataFrame to a CSV file
+    df.to_csv(f'./data/water-quality/analysispc_{year}.csv', index=False)
+
+    return status_code
+
 def get_operationpc_location(latitude, longitude, distance):
     """
     Makes a GET request to the specified endpoint with optional query parameters.
@@ -124,7 +175,8 @@ def get_stationpc_location(latitude, longitude, distance):
     params = {
         "distance": distance,
         "latitude": latitude,
-        "longitude": longitude
+        "longitude": longitude,
+        "size": 20000
     }
     return get_stationpc(params)
 
@@ -151,25 +203,7 @@ def get_analysepc_location_date(latitude, longitude, distance, date):
     return get_analysepc(params)
 
 def main():
-    params = {
-        "distance": "5",
-        "latitude": "45.782",
-        "longitude": "4.893",
-        "sort": "desc"
-    }
-
-    print(f"Calling Analyse Physico-Chimique , response code is : {get_analysepc(params)}")
-
-    print(f"Calling Operation Physico-Chimique, response code is : {get_operationpc(params)}")
-
-    #For stationpc we don't need to sort because the sort is on The Date of the last measurement
-    print(f"Calling Station Physico-Chimique, responde code is : {get_stationpc(params)}")
-
-    print(get_analysepc_location(45.782, 4.893, 5))
-    print(get_operationpc_location(45.782, 4.893, 5))
-    print(get_stationpc_location(45.782, 4.893, 5))
-
-    print(get_analysepc_location_date(45.782, 4.893, 5, "2022-09-01"))
+    print(get_analysepc_filtered_year(2020,"1319,1350,1383,1386"))
 
 if __name__ == "__main__":
     main()
