bbb07cb086676f04179b497c9fff8f8e5a1f6b4b
LAPTOP-F4TOHIAV\Tom <tom.delaporte@insa-lyon.fr>
2023-11-12 18:00:45 +0100

    Development of the ingestion pipeline

--- dags/air_quality/air_quality.py
+++ dags/air_quality/air_quality.py
@@ -1,4 +1,5 @@
 import os
+import glob
 
 from io import StringIO, BytesIO
 from typing import List
@@ -7,6 +8,8 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 
+LANDING_ZONE_PATH = "/opt/airflow/dags/data/landing/air-quality/"
+
 def download_stations_details():
     """Download a CSV file containing tons of details about every measuring station used to acquire data.
 
@@ -14,7 +17,7 @@ def download_stations_details():
     """
     r = requests.get("https://static.data.gouv.fr/resources/donnees-temps-reel-de-mesure-des-concentrations-de-polluants-atmospheriques-reglementes-1/20230808-065455/fr-2023-d-lcsqa-ineris-20230717.xls")
     station_metadata_df = pd.read_excel(BytesIO(r.content))
-    station_metadata_df.to_csv("data/air-quality/stations_metadata.csv", index=False)
+    station_metadata_df.to_csv(LANDING_ZONE_PATH+"stations_metadata.csv", index=False)
 
 def download_daily_reports(years: List[int]):
     """ Download daily measures data for all measuring stations and given years.
@@ -25,13 +28,14 @@ def download_daily_reports(years: List[int]):
     Measure reports are saved to Parquet format in data/air-quality/{YEAR}/FR_E2_{YYYY}_{MM}_{DD}. Example: FR_E2_2023_03_15
     """
     for year in years:
-        if not os.path.exists(f"data/air-quality/{year}"):
-            os.makedirs(f"data/air-quality/{year}")
+        if not os.path.exists(LANDING_ZONE_PATH+f"{year}"):
+            os.makedirs(LANDING_ZONE_PATH+f"{year}")
         r = requests.get(f"https://files.data.gouv.fr/lcsqa/concentrations-de-polluants-atmospheriques-reglementes/temps-reel/{year}")
         soup = BeautifulSoup(r.content, features="html.parser")
         for a in soup.select('a[href*=csv]'):
             filename = a['href']
-            r = requests.get(f"https://files.data.gouv.fr/lcsqa/concentrations-de-polluants-atmospheriques-reglementes/temps-reel/{year}/{filename}")
-            decoded_file = StringIO(r.content.decode("utf-8"))
-            report_df = pd.read_csv(decoded_file, sep=";")
-            report_df.to_parquet(f"data/air-quality/{year}/{filename[:-3]}")
+            if not os.path.exists(LANDING_ZONE_PATH+f"{year}/{filename[:-4]}"):
+                r = requests.get(f"https://files.data.gouv.fr/lcsqa/concentrations-de-polluants-atmospheriques-reglementes/temps-reel/{year}/{filename}")
+                decoded_file = StringIO(r.content.decode("utf-8"))
+                report_df = pd.read_csv(decoded_file, sep=";")
+                report_df.to_parquet(LANDING_ZONE_PATH+f"{year}/{filename[:-4]}")
--- dags/ingest.py
+++ dags/ingest.py
@@ -1,10 +1,12 @@
 from datetime import datetime
 
 from airflow import DAG
-from airflow.decorators import task
+from airflow.decorators import task, task_group
 from airflow.operators.empty import EmptyOperator
+from airflow.operators.python import PythonOperator
 
 import georisques.download as georisques_download
+from air_quality.air_quality import download_stations_details, download_daily_reports
 
 with DAG(
     dag_id='ingest',
@@ -12,10 +14,30 @@ with DAG(
     schedule=None # None means manually triggered
 ) as dag:
 
-    start = EmptyOperator(task_id="start")
+    start = EmptyOperator(task_id="start_ingestion")
+
+    @task_group(group_id="air_quality_ingestion")
+    def air_quality_ingestion():
+        get_air_quality_stations_details = PythonOperator(task_id="get_air_quality_stations_details",
+                                                          python_callable=download_stations_details
+                                                          )
+
+        get_air_quality_measures = PythonOperator(task_id="get_air_quality_measures",
+                                                  python_callable=download_daily_reports,
+                                                  op_kwargs={'years':[2021, 2022, 2023]})
+
+        get_air_quality_stations_details >> get_air_quality_measures
 
     @task
     def georisques():
-        print(georisques_download.get_name())
+        georisques_download.download_data()
 
-    start >> georisques()
+    @task_group(group_id="water_api_ingestion")
+    def water_api_ingestion():
+        get_water_api_data = EmptyOperator(task_id="get_water_api_data")
+    
+    end = EmptyOperator(task_id="ingestion_finished")
+
+    start >> georisques() >> end
+    start >> air_quality_ingestion() >> end
+    start >> water_api_ingestion() >> end
