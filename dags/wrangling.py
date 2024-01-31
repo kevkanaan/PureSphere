d44853de691a7f398b277a4ce06e52dd6297e0e3
tom_delaporte <94459384+Tom-DELAPORTE@users.noreply.github.com>
2024-01-31 20:47:53 +0100

    Commit cleaned air quality data to Postgres (#16)
    Add new steps to wrangling dag and store cleaned air quality data inside staging Postgres database

--- dags/wrangling.py
+++ dags/wrangling.py
@@ -7,12 +7,12 @@ from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 
-from air_quality.wrangling import remove_invalid_measurements, drop_useless_columns, aggregate_measurement_by_site_code_and_pollutant_type
+from air_quality.wrangling import remove_invalid_measurements, drop_useless_columns, aggregate_measurement_by_site_code_and_pollutant_type, create_air_quality_station_sql_table, create_air_quality_measurements_sql_table
 
 with DAG(
     dag_id='wrangling',
     start_date=datetime.now(),
-    schedule=None, # None means manually triggered
+    schedule=None, # None means manually triggered,
 ) as dag:
 
     start = EmptyOperator(task_id="start_wrangling")
@@ -24,19 +24,31 @@ with DAG(
                                  bash_command="cp -f /opt/airflow/data/landing/air-quality/stations_metadata.csv /opt/airflow/data/staging/air-quality")
 
         second_step = PythonOperator(task_id="remove_invalid_measurements",
-                                    python_callable=remove_invalid_measurements)
+                                    python_callable=remove_invalid_measurements,
+                                    trigger_rule="all_success")
 
         third_step = PythonOperator(task_id="drop_useless_columns",
-                                    python_callable=drop_useless_columns)
+                                    python_callable=drop_useless_columns,
+                                    trigger_rule="all_success")
 
         fourth_step = PythonOperator(task_id="aggregate_measurement_by_site_code_and_pollutant_type",
-                                    python_callable=aggregate_measurement_by_site_code_and_pollutant_type)
+                                    python_callable=aggregate_measurement_by_site_code_and_pollutant_type,
+                                    trigger_rule="all_success")
 
         fifth_step = SparkSubmitOperator(task_id="merge_daily_files",
                                           conn_id="spark-conn",
-                                          application="/opt/airflow/jobs/air-quality/merge_daily_reports.py")
+                                          application="/opt/airflow/jobs/air-quality/merge_daily_reports.py",
+                                          trigger_rule="all_success")
 
-        first_step >> second_step >> third_step >> fourth_step >> fifth_step
+        sixth_step = PythonOperator(task_id="create_air_quality_stations_sql_table",
+                                      python_callable=create_air_quality_station_sql_table,
+                                      trigger_rule="all_success")
+
+        seventh_step = PythonOperator(task_id="create_air_quality_measurements_sql_table",
+                                   python_callable=create_air_quality_measurements_sql_table,
+                                   trigger_rule="all_success")
+
+        first_step >> second_step >> third_step >> fourth_step >> fifth_step >> sixth_step >> seventh_step
 
     @task_group(group_id="georisque_wrangling")
     def georisques_wrangling():
